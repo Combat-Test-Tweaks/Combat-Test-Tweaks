bb65e9aeeb6c67123ee02de67717f819da2d7db2
Blumbo <blumboman@gmail.com>
2022-03-11 18:06:09 +0200

    Nerfed strength effect

--- src/main/java/net/blumbo/armortweaks/mixin/PlayerMixin.java
+++ src/main/java/net/blumbo/armortweaks/mixin/PlayerMixin.java
@@ -1,5 +1,6 @@
 package net.blumbo.armortweaks.mixin;
 
+import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
@@ -15,6 +16,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.minecraft.world.entity.player.Inventory;
 
 @Mixin(Player.class)
 public abstract class PlayerMixin extends LivingEntity {
@@ -25,40 +27,55 @@ public abstract class PlayerMixin extends LivingEntity {
 
     @Shadow public abstract Iterable<ItemStack> getHandSlots();
 
-    // If victim is in water check
+    // Check for if attacked is in water
     boolean inWater = false;
     @Inject(at = @At("HEAD"), method = "attack")
     protected void attack(Entity entity, CallbackInfo ci) {
         inWater = entity.isInWater();
     }
 
+    // Modify weapon and strength effect damage
     @Redirect(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;getAttributeValue(Lnet/minecraft/world/entity/ai/attributes/Attribute;)D"))
     protected double setAttackDamage(Player instance, Attribute attribute) {
 
-        double value = this.getAttributes().getValue(attribute);
+        double damage = this.getAttributes().getValue(attribute);
 
         ItemStack itemStack = getMainHandItem();
-        if (itemStack == null) return value;
+        if (itemStack == null) return damage;
 
         Item item = itemStack.getItem();
 
-        // Buff swords and axes to what they were in 1.16, I think picks and shovels worked differently but when asked
+        // Buff swords and tools
         if (item instanceof SwordItem || item instanceof AxeItem ||
                 item instanceof PickaxeItem || item instanceof ShovelItem) {
             String type = item.toString().split("_")[0];
-            if (!type.equals("wooden") && !type.equals("golden")) value += 1;
+            if (!type.equals("wooden") && !type.equals("golden")) damage += 1;
+        }
+
+        // Buff sharpness enchantment
+        int sharpness = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.SHARPNESS, itemStack);
+        if (sharpness > 0) {
+            damage += -0.5 + sharpness * 0.5;
         }
 
         // Buff trident damage, nerf impaling
         if (item instanceof TridentItem) {
-            value += 1;
+            damage += 1;
             int impaling = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.IMPALING, itemStack);
             if (inWater && impaling > 0) {
-                value += 1 - impaling * 1.5;
+                damage -= impaling;
             }
         }
 
-        return value;
+        // Buff strength effect
+        if (hasEffect(MobEffect.byId(5))) {
+            int strength = 1 + getEffect(MobEffect.byId(5)).getAmplifier();
+            damage /= 1 + strength * 0.2F;
+            damage *= 1 + strength * 0.3F;
+        }
+
+        return damage;
+
     }
 
 
